d68b47484bb65dca4441a7e53284e421efb3219f
Fanny <34095626+Nyayic@users.noreply.github.com>
2023-03-31 14:55:17 +0300

    created a drunkit using html css and js

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+function playSound(event){
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+    if(!audio) return;
+
+    key.classList.add('playing')
+    audio.currentTime = 0;
+    audio.play();
+}
+// trigger our property
+function removeTransition(event){
+    if(event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key=>key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
